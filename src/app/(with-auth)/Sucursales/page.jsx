b1cf5adbec9cecb0d59fb6d9172ceec499e22812
raulDeDevWorld <proyection009@gmail.com>
2023-09-30 22:11:06 -0400

    DC

--- src/app/(with-auth)/Sucursales/page.jsx
+++ src/app/(with-auth)/Sucursales/page.jsx
@@ -129,12 +129,11 @@ function Home() {
                 </table>
 
                 <div className='lg:flex hidden lg:fixed top-[100px] right-[65px] '>
-                    <div className='flex justify-center items-center h-[50px] text-white text-[14px] font-normal font-medium bg-[#32CD32] border border-gray-200 rounded-[10px] px-10 cursor-pointer mr-2' onClick={redirect}>Agregar Sucursal</div>
-                    <div className='flex justify-center items-center bg-[#0064FA] h-[50px] w-[50px]  rounded-full text-white cursor-pointer' onClick={redirect}> <span className='text-white text-[30px]'>+</span> </div>
+                    <div className='flex justify-center items-center h-[50px] text-white text-[14px] font-normal font-medium bg-[#00E2FF] border border-gray-200 rounded-[10px] px-10 cursor-pointer mr-2' onClick={redirect}>Agregar Sucursal</div>
+                    <div className='flex justify-center items-center bg-[#00095F] h-[50px] w-[50px]  rounded-full text-white cursor-pointer' onClick={redirect}> <span className='text-white text-[30px]'>+</span> </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
